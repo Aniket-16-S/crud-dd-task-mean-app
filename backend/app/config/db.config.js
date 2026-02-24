dd4c1d23197665e2b81f3cd0212686bcf6e61ce7
Aniket-16-S <aniket.sandye@gmail.com>
2026-02-24 10:44:34 +0530

    Added, Dockerfile for backend and frontend. images are pushed on docker hub Added docker compose yml at root dir testing is done. works as expected.

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//   url: "mongodb://admin:admin123@localhost:27017/dd_db?authSource=admin"
+// };
+
 module.exports = {
-  url: "mongodb://admin:admin123@localhost:27017/dd_db?authSource=admin"
+  url: process.env.MONGO_URI || "mongodb://localhost:27017/crud_db"
 };
